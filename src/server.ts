8e163c78076f329bae8675afc33d10e09b513e50
ValMai12 <lera.maevskaa@gmail.com>
2026-03-27 22:04:13 +0200

    Додала controller, router та опис проєкту

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-
-// import bookRouter from "./routes/book.router";
+import bookRouter from './routes/book.router';
 
 const app = express();
 
 app.use(express.json());
 
-// example:
-// app.use("/api/books", bookRouter);
+app.use('/api/books', bookRouter);
 
 export default app;
