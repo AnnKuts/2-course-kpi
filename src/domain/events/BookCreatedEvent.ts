0a8212e2f90f5a9168390382b1796b2a6ada2c3e
Yuliia <julia.kostandi@gmail.com>
2026-04-30 13:25:41 +0300

    feat: add in-memory event bus infrastructure and BookCreatedEvent

---
+++ src/domain/events/BookCreatedEvent.ts
@@ -0,0 +1,14 @@
+import { IIntegrationEvent } from '../../application/events/EventContracts';
+
+export class BookCreatedEvent implements IIntegrationEvent {
+  public readonly eventName = 'BookCreatedEvent';
+  public readonly occurredAt: Date;
+
+  constructor(
+    public readonly bookId: number,
+    public readonly title: string,
+    public readonly author: string
+  ) {
+    this.occurredAt = new Date();
+  }
+}
